89b6a87a7d99e7b070067a3bd3adbddf50803128
rizalfrm <rzlfrm.frm@gmail.com>
2017-03-23 11:42:18 +0700

    Update bidang.php

--- bidang.php
+++ bidang.php
@@ -24,7 +24,7 @@ class Bidang extends CI_Controller {
 		'kepalabidang' => $this->model_kepalabidang->kepalabidang(),
 		'kepalabidang_selected' => $this->input->post('tb_kepalabidang') ? $this->input->post('tb_kepalabidang') : '', // untuk edit ganti '' menjadi data dari database misalnya $row->provinsi
 	);
-		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; //judul title
+		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; 
         $data['jlhnotif'] =$this->model_notifikasi->notif_count();  //menghitung jumlah post
         $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi postingan
 		$this->load->view('admin/form_tambahbidang', $data);
